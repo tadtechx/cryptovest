317594be35075e3461c7d096b759fc498b01ff2a
eustacedike <eustacedyke@gmail.com>
2023-04-07 14:10:11 +0100

    commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 
 app.use(cors());
 app.use((req,res,next)=>{
-  res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
+  res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','https://cryptovest-front.vercel.app/');
   if(req.method === 'OPTIONS') {
       res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
       return res.status(200).json({});
