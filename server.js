4fc1cd2ee9cd8111fffd7dbbedf3b0a946859266
eustacedike <eustacedyke@gmail.com>
2023-04-07 14:06:53 +0100

    commit

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors());
+app.use((req,res,next)=>{
+  res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
+  if(req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+      return res.status(200).json({});
+  }
+  next();
+});
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
